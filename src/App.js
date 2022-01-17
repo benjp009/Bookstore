e1ecf39e8d2a6820b6c114053cc99aff9434a9f4
benjp009 <patin.benjamin@gmail.com>
2022-01-17 12:23:08 +0100

    Link page correctly

--- src/App.js
+++ src/App.js
@@ -11,39 +11,26 @@ import Categories from './redux/categories/Categories';
 // Design
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Books</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Books</Link>
+            </li>
+            <li>
+              <Link to="categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
 
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Routes>
-          <Route exact path="/">
-            <Books />
-          </Route>
-          <Route path="/categories">
-            <Categories />
-          </Route>
+          <Route path='/' element={<Books />} />
+          <Route path='categories' element={<Categories />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
-export default App;
+};
