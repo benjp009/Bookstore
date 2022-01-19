a32276d3d757f2b596b85850861f83acaeb0c503
benjp009 <patin.benjamin@gmail.com>
2022-01-19 12:39:27 +0100

    fix indent

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -10,17 +10,17 @@ const BookList = () => {
     <div className="book-display">
       <h2 className="title">List of Books</h2>
       <div className="new-book">
-      <ul>
-        {books.map((book) => (
-          <Book
-            key={book.id}
-            author={book.author}
-            title={book.title}
-            id={book.id}
-            category={book.category}
-          />
-        ))}
-      </ul>
+        <ul>
+          {books.map((book) => (
+            <Book
+              key={book.id}
+              author={book.author}
+              title={book.title}
+              id={book.id}
+              category={book.category}
+            />
+          ))}
+        </ul>
       </div>
       <Form />
     </div>
